ffc224246023cdad238fe9ccc85a7b34fd9ed2c1
Dhanus M Lal <dhanusmlal@gmail.com>
2022-06-18 21:46:01 +0530

    final change to script?

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # get the certificate
 mkdir /certs/
-curl https://raw.githubusercontent.com/DhanusML/arch-iiscwlan-config/master/IISc-DC1-CA.crt > /cert/IISc-DC1-CA.crt
+curl https://raw.githubusercontent.com/DhanusML/arch-iiscwlan-config/master/IISc-DC1-CA.crt > /certs/IISc-DC1-CA.crt
 
 # get the iwd.config file
 curl https://raw.githubusercontent.com/DhanusML/arch-iiscwlan-config/master/main.conf > /etc/iwd/main.conf
