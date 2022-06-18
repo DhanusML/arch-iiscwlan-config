874b7731b426a68a365fd41add94a1387fa5a3ec
Dhanus M Lal <dhanusmlal@gmail.com>
2022-06-18 21:17:11 +0530

    corrected script

--- script.sh
+++ script.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
 # get the certificate
 mkdir /cert/
-curl https://raw.githubusercontent/DhanusML/arch-iiscwlan-config/master/IISc-DC1-CA.crt > /cert/IISc-DC1-CA.crt
+curl https://raw.githubusercontent.com/DhanusML/arch-iiscwlan-config/master/IISc-DC1-CA.crt > /cert/IISc-DC1-CA.crt
 
 # get the iwd.config file
-curl https://raw.githubusercontent/DhanusML/arch-iiscwlan-config/master/main.conf > /etc/iwd/main.conf
+curl https://raw.githubusercontent.com/DhanusML/arch-iiscwlan-config/master/main.conf > /etc/iwd/main.conf
 
 # get the network.config file
-curl https://raw.githubusercontent/DhanusML/arch-iiscwlan-config/master/iiscwlan.8021x > /var/lib/iwd/iiscwlan.8021x
+curl https://raw.githubusercontent.com/DhanusML/arch-iiscwlan-config/master/iiscwlan.8021x > /var/lib/iwd/iiscwlan.8021x
